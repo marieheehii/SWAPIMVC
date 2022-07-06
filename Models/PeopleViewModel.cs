64489a5f854c1ab1a5e14bedccb84d0df2dacca2
Hannah Young <hamyoun@iu.edu>
2022-07-05 21:12:36 -0400

    UpdatedModels People and results

---
+++ Models/PeopleViewModel.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text.Json.Serialization;
+
+    public class PeopleViewModel
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+
+        [JsonPropertyName("height")]
+        public string Height { get; set; }
+
+        [JsonPropertyName("mass")]
+        public string Mass { get; set; }
+
+        [JsonPropertyName("birth_year")]
+        public string BirthYear { get; set; }
+        
+        [JsonPropertyName("url")]
+        public string Url { get; set; }
+
+        public string Id
+        {
+            get
+            {
+                return Url
+                .Split("/", StringSplitOptions.RemoveEmptyEntries)
+                .LastOrDefault();
+            }
+        }
+        
+    }
